af927a3957d2e91c1f6ca9dfaa2a76988c82f60c
praban <pbanawalkar2@gmail.com>
2026-01-19 18:44:32 +0530

    Section 7: Authentication - Supabase Authentication & Auth Store

---
+++ src/stores/storeAuth.js
@@ -0,0 +1,14 @@
+import { defineStore } from "pinia";
+import supabase from "src/config/supabase";
+
+export const useStoreAuth = defineStore("auth", () => {
+  /*
+    actions
+  */
+
+  /*
+    return
+  */
+
+  return {};
+});
